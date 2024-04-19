958e6f389be61b0569a1de487f6e7589aba176ee
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-04-19 14:49:40 -0500

    Add testing. WIP

---
+++ RMC DOTS/Scripts/Tests/PlayMode/Systems/PhysicsTrigger/PhysicsTriggerSystemTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Roguelite.Tests.Infrastructure;
+
+namespace RMC.DOTS.Systems.PhysicsTrigger
+{
+    [Category(nameof(PhysicsTriggerSystem))]
+    [TestFixture]
+    public class PhysicsTriggerSystemTest : CustomEcsTestsFixture
+    {
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+            CreateSystem<PhysicsTriggerSystem>();
+            CreateEntity(typeof(PhysicsTriggerSystemAuthoring.PhysicsTriggerSystemIsEnabledTag));
+        }
+        
+        [Test]
+        public void AfterUpdate_LoneEntity_HasNoPhysicsTriggerComponent()
+        {
+            // Create an entity with default components
+            var entity = CreateEntity(typeof(PhysicsTriggerComponent));
+            
+            // Add components with custom data
+            var component = new PhysicsTriggerComponent
+            {   
+                MemberOfLayerMask = 1,
+                CollidesWithLayerMask = 1
+            };
+            Manager.AddComponentData(entity, component);
+ 
+            // Update the system
+            UpdateSystem<PhysicsTriggerSystem>();
+ 
+            // Assert that data is modified according to the test case
+            var hasComponent = Manager.HasComponent<PhysicsTriggerOutputTag>(entity);
+            Assert.That(hasComponent, Is.False);
+        }
+    }
+}
