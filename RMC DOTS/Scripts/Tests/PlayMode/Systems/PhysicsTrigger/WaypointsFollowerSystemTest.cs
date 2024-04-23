370b6bbfccc23179fddc4f079c60183239f3035b
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-04-23 16:55:09 -0500

    0.6.0

--- RMC DOTS/Scripts/Tests/PlayMode/Systems/PhysicsTrigger/WaypointsFollowerSystemTest.cs
+++ RMC DOTS/Scripts/Tests/PlayMode/Systems/PhysicsTrigger/WaypointsFollowerSystemTest.cs
@@ -1,40 +1,40 @@
-﻿using NUnit.Framework;
-using Roguelite.Tests.Infrastructure;
-
-namespace RMC.DOTS.Systems.PhysicsTrigger
-{
-    [Category(nameof(WaypointsSystem))]
-    [TestFixture]
-    public class WaypointsFollowerSystemTest : CustomEcsTestsFixture
-    {
-        [SetUp]
-        public override void Setup()
-        {
-            base.Setup();
-            CreateSystem<WaypointsSystem>();
-            CreateEntity(typeof(PhysicsTriggerSystemAuthoring.PhysicsTriggerSystemIsEnabledTag));
-        }
-        
-        [Test]
-        public void AfterUpdate_LoneEntity_HasNoPhysicsTriggerComponent()
-        {
-            // Create an entity with default components
-            var entity = CreateEntity(typeof(PhysicsTriggerComponent));
-            
-            // Add components with custom data
-            var component = new PhysicsTriggerComponent
-            {   
-                MemberOfLayerMask = 1,
-                CollidesWithLayerMask = 1
-            };
-            Manager.AddComponentData(entity, component);
- 
-            // Update the system
-            UpdateSystem<WaypointsSystem>();
- 
-            // Assert that data is modified according to the test case
-            var hasComponent = Manager.HasComponent<PhysicsTriggerOutputComponent>(entity);
-            Assert.That(hasComponent, Is.False);
-        }
-    }
-}
+﻿// using NUnit.Framework;
+// using Roguelite.Tests.Infrastructure;
+//
+// namespace RMC.DOTS.Systems.PhysicsTrigger
+// {
+//     [Category(nameof(WaypointsSystem))]
+//     [TestFixture]
+//     public class WaypointsFollowerSystemTest : CustomEcsTestsFixture
+//     {
+//         [SetUp]
+//         public override void Setup()
+//         {
+//             base.Setup();
+//             CreateSystem<WaypointsSystem>();
+//             CreateEntity(typeof(PhysicsTriggerSystemAuthoring.PhysicsTriggerSystemIsEnabledTag));
+//         }
+//         
+//         [Test]
+//         public void AfterUpdate_LoneEntity_HasNoPhysicsTriggerComponent()
+//         {
+//             // Create an entity with default components
+//             var entity = CreateEntity(typeof(PhysicsTriggerComponent));
+//             
+//             // Add components with custom data
+//             var component = new PhysicsTriggerComponent
+//             {   
+//                 MemberOfLayerMask = 1,
+//                 CollidesWithLayerMask = 1
+//             };
+//             Manager.AddComponentData(entity, component);
+//  
+//             // Update the system
+//             UpdateSystem<WaypointsSystem>();
+//  
+//             // Assert that data is modified according to the test case
+//             var hasComponent = Manager.HasComponent<PhysicsTriggerOutputComponent>(entity);
+//             Assert.That(hasComponent, Is.False);
+//         }
+//     }
+// }
