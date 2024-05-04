c1a43a32552a08591f91ba6d2ad26f8d03e14eef
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-05-03 22:17:46 -0500

    Add static method to destroy more easily

--- RMC DOTS/Scripts/Runtime/Systems/DestroyEntity/DestroyEntitySystem.cs
+++ RMC DOTS/Scripts/Runtime/Systems/DestroyEntity/DestroyEntitySystem.cs
@@ -1,6 +1,7 @@
 ﻿using RMC.DOTS.SystemGroups;
 using Unity.Burst;
 using Unity.Entities;
+using UnityEngine;
 
 namespace RMC.DOTS.Systems.DestroyEntity
 {
@@ -34,5 +35,36 @@ namespace RMC.DOTS.Systems.DestroyEntity
                 }
             }
         }
+
+        /// <summary>
+        /// Experimental idea. This allows any scope to destroy an entity with a delay
+        /// while respecting if an existing component already exists.
+        ///
+        /// NOTE: 1. I tried to pass in less parameters. Failed.Perhaps doable.
+        /// NOTE: 2. I tried to make it non-static and lookup the system. Failed. Perhaps doable.
+        /// 
+        /// </summary>
+        public static void DestroyEntity(
+            ref EntityCommandBuffer ecb,
+            ComponentLookup<DestroyEntityComponent> _destroyEntityComponentLookup, 
+            Entity entity,
+            float timeTillDestroyInSeconds = -1)
+        {
+            bool hasDestroyEntityComponent = _destroyEntityComponentLookup.HasComponent(entity);
+
+            if (!hasDestroyEntityComponent)
+            {
+                ecb.AddComponent<DestroyEntityComponent>(entity, new DestroyEntityComponent
+                {
+                    TimeTillDestroyInSeconds = timeTillDestroyInSeconds
+                });
+            }
+            else
+            {
+                var destroyEntityComponent = _destroyEntityComponentLookup.GetRefRW(entity);
+                destroyEntityComponent.ValueRW.TimeTillDestroyInSeconds = timeTillDestroyInSeconds;
+            }
+        }
+
     }
 }
