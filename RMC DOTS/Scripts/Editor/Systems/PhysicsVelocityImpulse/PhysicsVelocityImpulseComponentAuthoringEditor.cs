f37a795825424804f33edd663e3ca414353b31a3
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-05-12 15:46:18 -0500

    Add demo of UIToolkit Custom Editor for PhysicsVelocity

---
+++ RMC DOTS/Scripts/Editor/Systems/PhysicsVelocityImpulse/PhysicsVelocityImpulseComponentAuthoringEditor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace RMC.DOTS.Systems.PhysicsVelocityImpulse
+{
+    [CustomEditor(typeof(PhysicsVelocityImpulseComponentAuthoring))]
+    public class PhysicsVelocityImpulseComponentAuthoringEditor : Editor
+    {
+        public VisualTreeAsset VisualTreeAsset;
+        private VisualElement _visualElement;
+        private PropertyField _isRandomForcePropertyField;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            _visualElement = new VisualElement();
+            VisualTreeAsset.CloneTree(_visualElement);
+
+            // Listen
+            _isRandomForcePropertyField = _visualElement.Q<PropertyField>("IsRandomForcePropertyField");
+            _isRandomForcePropertyField.RegisterCallback<ClickEvent>(IsRandomForcePropertyField_OnClick);
+            IsRandomForcePropertyField_OnClick(null);
+            return _visualElement;
+        }
+
+        private void IsRandomForcePropertyField_OnClick(ClickEvent evt)
+        {
+            var isRandomForceProperty = _isRandomForcePropertyField.bindingPath;
+            var isRandomForcePropertySerializedProperty = serializedObject.FindProperty(isRandomForceProperty);
+
+            var nonRandomVisualElement = _visualElement.Q<VisualElement>("NonRandomVisualElement");
+            var randomVisualElement = _visualElement.Q<VisualElement>("RandomVisualElement");
+
+            if (isRandomForcePropertySerializedProperty.boolValue)
+            {
+                nonRandomVisualElement.style.display = DisplayStyle.None;
+                randomVisualElement.style.display = DisplayStyle.Flex;
+            }
+            else
+            {
+                nonRandomVisualElement.style.display = DisplayStyle.Flex;
+                randomVisualElement.style.display = DisplayStyle.None;
+            }
+        }
+    }
+}
--- RMC DOTS/Scripts/Runtime/Systems/PhysicsVelocityImpulse/PhysicsVelocityImpulseComponentAuthoring.cs
+++ RMC DOTS/Scripts/Runtime/Systems/PhysicsVelocityImpulse/PhysicsVelocityImpulseComponentAuthoring.cs
@@ -7,24 +7,49 @@ namespace RMC.DOTS.Systems.PhysicsVelocityImpulse
     {
         //If you don't want random, just put the same values for min/max and false for canBeNegative
         [Header("ApplyLinearImpulse System")]
-        public bool CanBeNegative = true;
-        public Vector3 MinForce = new Vector3(1,1, 0);
-        public Vector3 MaxForce = new Vector3(1,1, 0);
         
-        public class PhysicsVelocityImpulseComponentAuthoringBaker : Baker<PhysicsVelocityImpulseComponentAuthoring>
+        public bool IsRandomForce = false;
+        
+        // Not Random
+        public Vector3 Force = new Vector3(1,1, 0);
+        
+        // Random
+        public bool RandomCanBeNegative = true;
+        public Vector3 RandomMinForce = new Vector3(1,1, 0);
+        public Vector3 RandomMaxForce = new Vector3(1,1, 0);
+        
+         public class PhysicsVelocityImpulseComponentAuthoringBaker : Baker<PhysicsVelocityImpulseComponentAuthoring>
         {
             public override void Bake(PhysicsVelocityImpulseComponentAuthoring authoring)
             {
                 Entity entity = GetEntity(TransformUsageFlags.Dynamic);
 
-                AddComponent<PhysicsVelocityImpulseComponent>(entity,
-                    new PhysicsVelocityImpulseComponent
-                    {
-                        CanBeNegative = authoring.CanBeNegative,
-                        MinForce = authoring.MinForce,
-                        MaxForce = authoring.MaxForce
-                    });
+                if (authoring.IsRandomForce)
+                {
+                    //Pass along data for a random choice later
+                    AddComponent<PhysicsVelocityImpulseComponent>(entity,
+                        new PhysicsVelocityImpulseComponent
+                        {
+                            CanBeNegative = authoring.RandomCanBeNegative,
+                            MinForce = authoring.RandomMinForce,
+                            MaxForce = authoring.RandomMaxForce
+                        });
 
+                }
+                else
+                {
+                    
+                    //Pass along non-random values for a specific choice now
+                    AddComponent<PhysicsVelocityImpulseComponent>(entity,
+                        new PhysicsVelocityImpulseComponent
+                        {
+                            CanBeNegative = false,
+                            MinForce = authoring.Force,
+                            MaxForce = authoring.Force
+                        });
+
+                }
+    
             }
         }
     }
