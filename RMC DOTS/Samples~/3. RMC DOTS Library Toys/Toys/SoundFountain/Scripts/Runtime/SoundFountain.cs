4939708722a6a9782f57de681761c0cacb161735
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-05-14 21:49:27 -0500

    1.3.1 - More sounds for fountain

--- RMC DOTS/Samples~/3. RMC DOTS Library Toys/Toys/SoundFountain/Scripts/Runtime/SoundFountain.cs
+++ RMC DOTS/Samples~/3. RMC DOTS Library Toys/Toys/SoundFountain/Scripts/Runtime/SoundFountain.cs
@@ -1,3 +1,4 @@
+using RMC.Core.Utilities;
 using UnityEngine;
 
 namespace RMC.DOTS.Toys.Fountain
@@ -8,6 +9,11 @@ namespace RMC.DOTS.Toys.Fountain
         protected void Start()
         {
             Debug.Log("SoundFountain Demo. Watch the spawning.");
+            
+            // The Unity Project Must Have These Layers
+            // LayerMaskUtility Shows Errors If Anything Is Missing
+            LayerMaskUtility.AssertLayerMask("Floor", 15);
+            LayerMaskUtility.AssertLayerMask("Droplet", 16);
         }
     }
 }
--- RMC DOTS/Samples~/3. RMC DOTS Library Toys/Toys/SoundFountain/Scripts/Runtime/SoundFountainSystem.cs
+++ RMC DOTS/Samples~/3. RMC DOTS Library Toys/Toys/SoundFountain/Scripts/Runtime/SoundFountainSystem.cs
@@ -1,5 +1,6 @@
 ﻿using RMC.Audio.Data.Types;
 using RMC.DOTS.Systems.Audio;
+using RMC.DOTS.Systems.PhysicsTrigger;
 using RMC.DOTS.Systems.PhysicsVelocityImpulse;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -8,6 +9,11 @@ using UnityEngine;
 
 namespace RMC.DOTS.Toys.Fountain
 {
+    /// <summary>
+    /// Detect trigger, EXACTLY ONE TIME
+    /// </summary>
+    public struct PhysicsTriggerExecuteOnceTag : IComponentData {}
+    
     /// <summary>
     /// When a new <see cref="Entity"/> droplet has a
     /// <see cref="PhysicsVelocityImpulseComponent"/> on it,
@@ -21,8 +27,13 @@ namespace RMC.DOTS.Toys.Fountain
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<PhysicsVelocityImpulseSystemAuthoring.PhysicsVelocityImpulseSystemIsEnabledTag>();
-            state.RequireForUpdate<PhysicsVelocityImpulseComponent>();
+            state.RequireForUpdate<PhysicsTriggerSystemAuthoring.PhysicsTriggerSystemIsEnabledTag>();
             state.RequireForUpdate<EndInitializationEntityCommandBufferSystem.Singleton>();
+            
+            //These are OPTIONAL...
+            //NOTE: Don't require PhysicsVelocityImpulseComponent
+            //NOTE: Don't require PhysicsTriggerOutputComponent
+
         }
 
         public void OnUpdate(ref SystemState state)
@@ -32,6 +43,7 @@ namespace RMC.DOTS.Toys.Fountain
                 CreateCommandBuffer(state.WorldUnmanaged);
             
             
+            // 1) Detect when something spawns, and play a sound
             foreach (var (physicsVelocity, physicsVelocityImpulseComponent, entity) in 
                      SystemAPI.Query<
                              RefRW<PhysicsVelocity>,
@@ -40,14 +52,41 @@ namespace RMC.DOTS.Toys.Fountain
             {
                 //NOTE: A key concept is that this system runs AFTER the PhysicsVelocityImpulseSystem
                 // So it can grab the following value which was JUST set.
-                var pitch = 0.5f + math.abs(physicsVelocity.ValueRO.Linear.y) / 10;
-                var volume = AudioConstants.VolumeDefault / 3;
+                var pitch1 = 0.5f + math.abs(physicsVelocity.ValueRO.Linear.y) / 10;
+                var volume1 = 0.1f;
                 
                 ecb.AddComponent<AudioComponent>(entity, 
                     new AudioComponent(
-                        "SoundFountain01", 
-                        volume,
-                        pitch));
+                        "Spawn01", 
+                        volume1,
+                        pitch1));
+            }
+            
+            // 2) Detect when something bounces, and play a sound
+            foreach (var (physicsVelocity, physicsTriggerOutputComponent, entity) in 
+                     SystemAPI.Query<
+                             RefRW<PhysicsVelocity>,
+                             RefRO<PhysicsTriggerOutputComponent>>().
+                         WithNone<PhysicsTriggerExecuteOnceTag>().
+                         WithEntityAccess())
+            {
+                if (physicsTriggerOutputComponent.ValueRO.PhysicsTriggerType != PhysicsTriggerType.Enter ||
+                    physicsTriggerOutputComponent.ValueRO.TimeFrameCountForLastCollision == Time.frameCount)
+                {
+                    var pitch2 = 0.5f + math.abs(physicsVelocity.ValueRO.Linear.y) / 10;
+                    var volume2 = AudioConstants.VolumeDefault;
+                    
+                    ecb.AddComponent<AudioComponent>(entity, 
+                        new AudioComponent(
+                            "Bounce01", 
+                            volume2,
+                            pitch2));
+
+                    //When the trigger rolls around on the floor eventually, it makes too many 'new' hits...
+                    //So only one sound EVER per droplet will be heard now
+                    ecb.AddComponent<PhysicsTriggerExecuteOnceTag>(entity);
+                }
+       
             }
         }
     }
