3cf3fc623b0b9891002b1e25639c30c39e207ec0
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-05-17 16:34:31 -0500

    1.4.9

--- RMC DOTS/Samples~/2. RMC DOTS Tweening/Tweening/MagicTween/Scripts/Runtime/MagicTween.cs
+++ RMC DOTS/Samples~/2. RMC DOTS Tweening/Tweening/MagicTween/Scripts/Runtime/MagicTween.cs
@@ -12,9 +12,14 @@ namespace RMC.DOTS.Tweening.MagicTween
         protected void Start()
         {
             Debug.Log("MagicTween Demo. Watch the spawning.");
-            
+
 #if !MAGICTWEEN_ENABLE_TRANSFORM_JOBS || !MAGICTWEEN_ENABLE_DEMO
-            Debug.LogWarning("Add scripting define for MAGICTWEEN_ENABLE_TRANSFORM_JOBS and MAGICTWEEN_ENABLE_DEMO to enable this demo.");
+
+            Debug.LogWarning("1. Add Magic Tween in package manager via https://github.com/AnnulusGames/MagicTween?tab=readme-ov-file#implementation-for-ecs.");
+
+            Debug.LogWarning("2. Add scripting define for MAGICTWEEN_ENABLE_TRANSFORM_JOBS");
+
+            Debug.LogWarning("3. Add scripting define for MAGICTWEEN_ENABLE_DEMO");
 #endif
             
         }
